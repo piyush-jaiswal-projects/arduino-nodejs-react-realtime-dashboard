1ca7e1718f6060dccf4b4f3942760dad83f6b5a4
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-05-07 19:08:19 +0530

    Update header.jsx

--- frontend/src/Container/header.jsx
+++ frontend/src/Container/header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='app-header flex flex-center'>
-            <h2 className='margin-0'>IOT DEVICE MONITORING</h2>
+            <h2 className='margin-0'>ARDUINO MONITORING</h2>
         </div>
     );
 }
